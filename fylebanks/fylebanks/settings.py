4ef2ae7ef09e7b2ec3a49f8404c3b9ca3452d2d7
Akshit Desai <akshitdesai2000@gmail.com>
2021-01-24 11:38:16 +0530

    [FIX] fixed allowed host

--- fylebanks/fylebanks/settings.py
+++ fylebanks/fylebanks/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['https://bankofindia-frontend.herokuapp.com/', 'http://localhost:3000/', '127.0.0.1']
 
 
 # Application definition
